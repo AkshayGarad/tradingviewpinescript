97d104a7446d6852a7b1a7daefb3ca18f890aca0
AkshayGarad <122161534+AkshayGarad@users.noreply.github.com>
2023-01-12 11:15:35 +0530

    Delete sample.sh

--- sample.sh
+++
@@ -1,7 +0,0 @@
-echo "hello good morning"
-echo "Car requires maintenance."
-echo "Car drives on the road."
-echo "Engine powers the car."
-echo "Car needs gas to run."
-echo "Brakes stop the car."
-echo "change done in car"
